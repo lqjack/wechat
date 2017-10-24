65b293f70ee3824ed1d80afffc1c1095bde0c945
lqjaclee <lqjacklee@126.com>
2017-10-24 23:16:26 +0800

    mine page

--- src/pages/person/person.ts
+++ src/pages/person/person.ts
@@ -15,11 +15,33 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class PersonPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController,
+    public navParams: NavParams
+    // public toastCtrl: ToastController
+  ) {
   }
 
   ionViewDidLoad() {
+
     console.log('ionViewDidLoad PersonPage');
+  }
+
+  saveQr() {
+    let that = this;
+    // navigator.screenshot.save(function (error, res) {
+    //   if (error) {
+    //     that.showToast("保存失败！");
+    //   } else {
+    //     that.showToast("保存成功，微信扫描关注公众号~");
+    //   }
+    // }, 'jpg', 100, 'Ion2Screenshot');
+  }
+  showToast(msg) {
+    // let toast = this.toastCtrl.create({
+    //   message: msg,
+    //   duration: 2000
+    // });
+    // toast.present();
   }
 
 }
